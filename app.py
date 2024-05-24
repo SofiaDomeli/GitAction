4d8ebc3344a2f3167b2aff26c3ed6ecb841600c1
Sofia Domingues <124214443+SofiaDomeli@users.noreply.github.com>
2024-05-24 07:27:01 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def devops():
- return '<center><h1><font color=red>Germinare Tech, EU AMO DEVOPS</center>'
+    return '<center><h1><font color=red>Germinare Tech, EU AMO DEVOPS</center>'
 
 @app.route('/Sofia')
 def devopsoso():
- return '<center><h1><font color=red>Germinare Tech, EU !AMO DEVOPS</center>'
+    return '<center><h1><font color=red>Germinare Tech, EU !AMO DEVOPS</center>'
 
 if __name__ == '__main__':
- app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
